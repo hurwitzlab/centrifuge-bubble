9589135eef4ec900c35d7a5f88ef08e857f96f7a
Ken Youens-Clark <kyclark@gmail.com>
2019-12-03 14:07:07 -0700

    moving forward

--- stampede/template.sh
+++ stampede/template.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "CENTRIFUGE_DIR  \"${CENTRIFUGE_DIR}\""
-echo "TITLE           \"${TITLE}\""
-echo "EXCLUDE_SPECIES \"${EXCLUDE_SPECIES}\""
-echo "MIN_PROPORTION  \"${MIN_PROPORTION}\""
-echo "MAX_SPECIES     \"${MAX_SPECIES}\""
+IMG="/work/05066/imicrobe/singularity/centrifuge-bubble-0.0.6.img"
 
-sh run.sh ${CENTRIFUGE_DIR} ${TITLE} ${EXCLUDE_SPECIES} ${MIN_PROPORTION} ${MAX_SPECIES}
+bash singularity exec "$IMG" plot.py ${QUERY} ${TITLE} ${EXCLUDE_SPECIES} ${MIN_PROPORTION} ${MAX_TAXA} ${TAX_RANK}
