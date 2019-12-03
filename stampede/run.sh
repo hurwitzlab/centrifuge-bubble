81dd9ef696de87e3e9b828a97560f2e4aee201c7
Ken Youens-Clark <kyclark@email.arizona.edu>
2019-12-03 16:16:04 -0600

    working

--- stampede/run.sh
+++ stampede/run.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "INVOCATION: " $0 $*
-
-module load tacc-singularity
-
 set -u
 
-IMG="/work/05066/imicrobe/singularity/centrifuge-bubble-0.0.5.img"
+IMG="/work/05066/imicrobe/singularity/centrifuge-bubble-0.0.6.img"
 
 singularity run "$IMG" "$@"
--- stampede/template.sh
+++ stampede/template.sh
@@ -2,4 +2,4 @@
 
 IMG="/work/05066/imicrobe/singularity/centrifuge-bubble-0.0.6.img"
 
-bash singularity exec "$IMG" plot.py ${QUERY} ${TITLE} ${EXCLUDE_SPECIES} ${MIN_PROPORTION} ${MAX_TAXA} ${TAX_RANK}
+singularity exec "$IMG" plot.py ${QUERY} ${TITLE} ${EXCLUDE_SPECIES} ${MIN_PROPORTION} ${MAX_TAXA} ${TAX_RANK}
