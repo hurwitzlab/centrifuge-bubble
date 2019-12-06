567bacbcbca71df03412cd788a569798b87e4e84
Ken Youens-Clark <kyclark@email.arizona.edu>
2019-12-06 15:10:30 -0600

    working

--- stampede/template.sh
+++ stampede/template.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 IMG="/work/05066/imicrobe/singularity/centrifuge-bubble-0.0.6.img"
 
 singularity exec "$IMG" plot.py ${QUERY} ${TITLE} ${EXCLUDE_SPECIES} ${MIN_PROPORTION} ${MAX_TAXA} ${TAX_RANK}
